0d1eeb79240525875ca1d782f8a5361811519c5d
omfgtora <spaminol@gmail.com>
2018-04-05 19:05:08 -0700

    Question vue component. Edit button now changes template to inputs.

--- app.js
+++ app.js
@@ -27,11 +27,76 @@ var data = {
   ]
 };
 
+Vue.component('question',{
+  props: ['question', 'answer'],
+  template: `
+    <div class="box" v-if="isEditable">
+      <div class="media-content">
+        <div class="content">
+
+          <div class="hero is-small">
+            <div class="hero-body">
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text" :value="question" @input="this.question = $event.target.value"></input>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" :value="answer" style="height: fit-content;"></textarea>
+            </div>
+          </div>
+
+          <div class="level-right"><button class="button is-success is-small" type="submit" @click="editQuestion(this.isEditable)">Save</button></div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="box" v-else>
+      <div class="media-content">
+        <div class="content">
+
+          <div class="hero is-small">
+            <div class="hero-body">
+              <h3 class="title">{{ question }}</h3>
+            </div>
+          </div>
+
+          <p>{{ answer }}</p>
+
+          <div class="level-right"><button class="button is-small" @click="editQuestion(this.isEditable)"><span class="icon-edit"></span></button></div>
+        </div>
+      </div>
+    </div>
+  `,
+
+  data(){
+    return {
+      isEditable: false
+    };
+  },
+
+  methods: {
+    editQuestion(){
+      if (this.isEditable === false) {
+        return this.isEditable = true
+      }
+      else {
+        return this.isEditable = false
+      }
+    }
+  }
+})
+
 new Vue({
   el: '#content',
   data: {
     newQuestion: {
-      "category": this.category,
+      //"category": this.category,
       "question": this.question,
       "answer": this.answer
     },
@@ -42,7 +107,7 @@ new Vue({
     addQuestion() {
       var newQuestion = this.newQuestion
       var addingQuestion = new Object();
-      addingQuestion.category = newQuestion.category;
+      //addingQuestion.category = newQuestion.category;
       addingQuestion.question = newQuestion.question;
       addingQuestion.answer =  newQuestion.answer;
       data.questions.push(addingQuestion);
@@ -53,7 +118,8 @@ new Vue({
       this.output = "var data = {\nquestions: "
         + JSON.stringify(this.questions, null, 2)
         + "\n}"
-    }
+    },
+
   },
   computed: {
     categorized: function() {
